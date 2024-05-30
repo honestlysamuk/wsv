f54b24e258921bb7b1425b884a78fca9f6aca63d
Samuel Arrowsmith <s.r.arrowsmith@pm.me>
2024-05-30 12:06:59 +0100

    Added a "calculation" function for fun

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -28,4 +28,35 @@ fn from_string_with_parser(i: &mut impl Read, parser: Parser) -> Vec<Result<Vec<
     buf.split('\n').enumerate().map(parser.fn_ptr()).collect()
 }
 
+pub fn calculate_stuff(input: &Vec<Result<Vec<WsvValue>, Error>>) -> String {
+    fn char_len(row: &Result<Vec<WsvValue>, Error>) -> usize {
+        match row {
+            Ok(data) => data.iter().map(|val| val.len()).sum(),
+            Err(_) => 1,
+        }
+    }
+    fn row_len(row: &Result<Vec<WsvValue>, Error>) -> usize {
+        match row {
+            Ok(data) => data.len(),
+            Err(_) => 1,
+        }
+    }
+
+    let character_count = input.iter().map(|row| char_len(row)).sum::<usize>();
+    let used_cell_count = input.iter().map(|row| row_len(row)).sum::<usize>();
+    let row_count = input.len();
+
+    use std::fmt::Write;
+    let mut out = String::new();
+    write!(
+        out,
+        "Minimum Byte Count: {}",
+        character_count + used_cell_count * row_count
+    )
+    .unwrap();
+    write!(out, "\nUsed Cell Count: {}", used_cell_count).unwrap();
+    write!(out, "\nRow Count: {}", row_count).unwrap();
+    out
+}
+
 mod benchmarks;
