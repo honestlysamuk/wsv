f082311b74ec19da31d07adb7f1551d24f34274d
Samuel Arrowsmith <s.r.arrowsmith@pm.me>
2024-05-30 12:08:18 +0100

    New comments benchmark

--- src/io/benchmarks.rs
+++ src/io/benchmarks.rs
@@ -14,13 +14,6 @@ use crate::data_model::Parser;
 use divan::black_box;
 
 #[divan::bench(args = PARSERS, sample_count = 1000)]
-fn reader_lines_100(parser: Parser) {
-    from_reader_with_parser(
-        black_box(&mut File::open("./tests/example_files/100lines.wsv").unwrap()),
-        parser,
-    );
-}
-#[divan::bench(args = PARSERS, sample_count = 1000)]
 fn reader_one_big_line(parser: Parser) {
     from_reader_with_parser(
         black_box(&mut File::open("./tests/example_files/1bigline.wsv").unwrap()),
@@ -34,28 +27,28 @@ fn reader_lines_100_000(parser: Parser) {
         parser,
     );
 }
-
-#[divan::bench(args = PARSERS, sample_count = 1000)]
-fn string_lines_100(parser: Parser) {
-    from_string_with_parser(
-        black_box(&mut File::open("./tests/example_files/100lines.wsv").unwrap()),
-        parser,
-    );
-}
-#[divan::bench(args = PARSERS, sample_count = 1000)]
-fn string_one_big_line(parser: Parser) {
-    from_string_with_parser(
-        black_box(&mut File::open("./tests/example_files/1bigline.wsv").unwrap()),
-        parser,
-    );
-}
 #[divan::bench(args = PARSERS, sample_count = 10)]
-fn string_lines_100_000(parser: Parser) {
-    from_string_with_parser(
-        black_box(&mut File::open("./tests/example_files/100000lines.wsv").unwrap()),
+fn reader_comments_100_000(parser: Parser) {
+    from_reader_with_parser(
+        black_box(&mut File::open("./tests/example_files/100000comments.wsv").unwrap()),
         parser,
     );
 }
+
+// #[divan::bench(args = PARSERS, sample_count = 1000)]
+// fn string_one_big_line(parser: Parser) {
+//     from_string_with_parser(
+//         black_box(&mut File::open("./tests/example_files/1bigline.wsv").unwrap()),
+//         parser,
+//     );
+// }
+// #[divan::bench(args = PARSERS, sample_count = 10)]
+// fn string_lines_100_000(parser: Parser) {
+//     from_string_with_parser(
+//         black_box(&mut File::open("./tests/example_files/100000lines.wsv").unwrap()),
+//         parser,
+//     );
+// }
 
 // count length of every string
 // sum every number
